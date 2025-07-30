5cbdfe079cd55af5bfd4a33753b308132dbf3753
e.hashemi <e.hashemi@samatco.ir>
2025-07-30 14:20:29 +0330

    contact page content structure added

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,12 @@
 export default function ShopPage() {
-    return <div>SHOP</div>
+    return <div>
+        <div className="w-full flex flex-col items-center justify-center md:flex-row md:items-start md:justify-between gap-4">
+        <div className="w-full md:basis-1/2 bg-green-200">
+            <div>Faqs</div>
+            <div>Address and information</div>
+        </div>
+        <div className="w-full md:basis-1/2 bg-gray-200">contact form</div>
+    </div>
+    <div className="w-full bg-blue-100 mt-4">map</div>
+    </div>
 }
