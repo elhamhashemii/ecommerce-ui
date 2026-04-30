cae6c1cd79d41cb5e85de995a5c136c274edf871
elhamhashemii <hashemi.elham22@gmail.com>
2026-04-30 14:04:20 +0330

    dealing with enamad

--- components/layouts/EnamadLogo.tsx
+++ components/layouts/EnamadLogo.tsx
@@ -1,20 +1,8 @@
 export default function EnamadLogo() {
     return <div className="w-full flex items-center justify-center mx-auto">
         <div className="w-24 h-24 my-4">
-            <a
-                // @ts-ignore
-                referrerpolicy="origin"
-                target="_blank"
-                href="https://trustseal.enamad.ir/?id=6011776&Code=rr4vd84z9kqDRUy54l34H64r>"
-            >
-                <img
-                    // @ts-ignore
-                    referrerpolicy="origin"
-                    src="https://trustseal.enamad.ir/logo.aspx?id=6011776&Code=rr4vd84z9kqDRU>"
-                    // style="cursor:pointer;"
-                    alt="enamad"
-                />
-            </a>
+            {/* @ts-ignore */}
+            <a referrerpolicy='origin' target='_blank' href='https://trustseal.enamad.ir/?id=721359&Code=rr4vd84z9kqDRUy54l34H64r2Vps9Idj'><img referrerpolicy='origin' src='https://trustseal.enamad.ir/logo.aspx?id=721359&Code=rr4vd84z9kqDRUy54l34H64r2Vps9Idj' alt='' style='cursor:pointer' code='rr4vd84z9kqDRUy54l34H64r2Vps9Idj'></a>
         </div>
     </div>
 }
